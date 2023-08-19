e78b3337976657bf6137d88d74471ebe0b301da2
Linar Samigullin <samigullinlinar@ya.ru>
2023-08-19 15:52:41 +0700

    after check with Saphire

--- gdis_kpd.py
+++ gdis_kpd.py
@@ -7,9 +7,6 @@ import math
 from mpmath import invertlaplace
 
 
-df = pd.read_excel("saphir2.xlsx", sheet_name="Лист2")
-
-
 # The Bourdet derivative
 def bourdet_derivative(p,t):
     n = np.size(p)
@@ -55,14 +52,27 @@ def Pwd_dimension(Pwd):
 
 
 def solve_kpd(Tinput, Qinput, xf, poro, h, k, S, Cs, Fcd, Pi, N=50):
-    q = -1  # unit flow rate m3/s
+    """
+
+    :param Tinput: массив времени, datetime
+    :param Qinput: массив закачек, np.array
+    :param xf: полудлина трещины, float
+    :param poro: коэффициент пористости, float
+    :param h: мощность пласта, float
+    :param k: проницаемость пласта, float
+    :param S: скин-фактор, float
+    :param Cs: wellbore storage коэффициент, float
+    :param Fcd: безразмерная проницаемость трещины, float
+    :param Pi: пластовое давление, float
+    :param N: число точек, int
+    :return: Pressure array, Time array
+    """
+
     Ct = 3e-6 / 6894.759  # total compressibility
     mu = 1e-3  # viscosity
-    B = 1  # volume factor
 
     # преобразование типа с дататайма в инт (секунды)
-    # T = np.array(Tinput, dtype=f"datetime64[s]").astype(np.int64)
-    T = Tinput
+    T = np.array(Tinput, dtype=f"datetime64[s]").astype(np.int64)
 
     # манипуляции с закачкой
     Q = np.zeros_like(Qinput)
@@ -78,8 +88,7 @@ def solve_kpd(Tinput, Qinput, xf, poro, h, k, S, Cs, Fcd, Pi, N=50):
     # массив времени
     tt = np.linspace(1, T[-1]+1, N)
 
-    # обезразмеривание времени и Cd
-    # tD = (k * tt) / (poro * mu * Ct * (xf ** 2))
+    # обезразмеривание и Cd
     Cd = Cs / (2 * np.pi * poro * Ct * h * (xf ** 2))
 
     # массив изначального давления
@@ -88,19 +97,15 @@ def solve_kpd(Tinput, Qinput, xf, poro, h, k, S, Cs, Fcd, Pi, N=50):
     # главный расчетный цикл по ступеням закачки
     for i in range(0, len(Q)):
         print(round(i / (len(Q)) * 100), "%")
-
         after = tt >= T[i]
         tt1 = tt[after] - T[i]
         td1 = (k * tt1) / (poro * mu * Ct * (xf ** 2))
         pwd1 = pwd(S, Cd, Fcd, td1)
         pwd1 = np.array(pwd1.tolist(), dtype=float)
         p[after] += Q[i] * Pwd_dimension(pwd1)
-
     print(100, "%")
 
-    # p - Pa
-    # tt - seconds
-
+    # обратный перевод в datetime
     time = pd.to_datetime(tt, unit='s')
 
     return p, time
@@ -109,21 +114,20 @@ def solve_kpd(Tinput, Qinput, xf, poro, h, k, S, Cs, Fcd, Pi, N=50):
 if __name__ == "__main__":
 
     # Parametrs
-    xf = 10 * 0.3048           # fracture half-length from ft to m
-
+    xf = 3.67 * 0.3048           # fracture half-length from ft to m
 
     poro = 0.1          # rock porosity
 
     h = 30 * 0.3048              # total formation thickness from ft to m
 
-    k = 321 * 1.02e-15          # permeability m2
-    S = -2               # skin factor
-    Cs = 0.0123 / 6894.759 * 0.158987            # wellbore storage coefficient
+    k = 386 * 1.02e-15          # permeability m2
+    S = 0.294684               # skin factor
+    Cs = 0.014 / 6894.759 * 0.158987            # wellbore storage coefficient
 
-    Fc = 2790 * 1.02e-15     # dimension fracture conductivity
-    Fcd = 0.5             # dimensionless fracture conductivity
+    Fc = 1420 * 1.02e-15     # dimension fracture conductivity
+    Fcd = 1             # dimensionless fracture conductivity
 
-    Pi = 3910.78 * 6894.759            # пластовое давление из psia в Па
+    Pi = 3910.03 * 6894.759            # пластовое давление из psia в Па
 
     Qinput = np.array([789, 850, 816, 2450, 2410, 976, 942, 920, 912, 865, 835, 830, 0, 0], dtype=float)    # закачка в м3/сут
     Tinput = np.array([0, 1.005, 2.393, 3.800, 5.167, 7.1, 9.484, 11.488, 13.414, 15.804, 18.501, 20.968, 23.55, 41.55])   # время в часах
@@ -131,21 +135,29 @@ if __name__ == "__main__":
     Qinput *= 0.158987
     Tinput *= 3600
 
-    N = 50                # кол-во расчетных точек
+    N = 100                # кол-во расчетных точек
 
     # применение функции solve_kpd
     pressure, time = solve_kpd(Tinput, Qinput, xf, poro, h, k, S, Cs, Fcd, Pi, N)
     print(pressure)
     print(time)
 
+    df = pd.read_excel("saphir2.xlsx", sheet_name="Лист2")
+
+    df_test = pd.read_excel("saphir_test2.xlsx", sheet_name="Лист1")
+
 
     fig, axs = plt.subplots(2, 1, figsize=[8,5])
     axs[0].plot(time, pressure, color='red', linewidth=0.5)
-    # axs[0].plot(time, np.ones_like(time)*Pi, color='blue', linewidth=1)
+    axs[0].plot(time, np.ones_like(pressure)*Pi, color='blue', linewidth=0.5)
     axs[1].step((Tinput), Qinput, where='post', color='orange', marker='o')
+
+    Tanaldata = pd.to_datetime(df_test['Time, hr'], unit='h')
+    axs[0].plot(Tanaldata, df_test['Pressure, psia'] * 6894.759 , color='green', linewidth=0.5)
+
 
     Trealdata = pd.to_datetime(df['t'], unit='h')
 
-    axs[0].plot(Trealdata, df['p, Pa'], color='blue', linewidth=0.5)
+    axs[0].plot(Trealdata, df['p, Pa'], color='blue', linewidth=1)
 
     plt.show()
