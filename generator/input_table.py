95736f83ead7352cf8bf35d03170787f63434ead
Alex1um <assassin20563@gmail.com>
2023-08-20 12:05:25 +0700

    begin upload modal generator

---
+++ generator/input_table.py
@@ -0,0 +1,48 @@
+from dash import Dash, Input, Output
+# from dash_core_components import Upload
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+
+def generate_upload_table(
+    app: Dash,
+    params_output: Output,
+    source_output: Output,
+    upload_component: dcc.Upload,
+    open_triggers: list[Input],
+    modal_inputs: list,
+) -> dbc.Modal:
+    
+    config_modal = dbc.Modal(
+    [
+        dbc.ModalHeader(
+            [
+                dbc.ModalTitle(
+                    "Configure Table",
+                ),
+            ],
+            close_button=False,
+        ),
+        dbc.ModalBody(
+            children=CONFIG_MODAL_CONTENT,
+        ),
+        dbc.ModalFooter(
+            [
+                BT_CANCEL := dbc.Button(
+                    "Cancel",
+                    color="secondary",
+                ),
+                BT_OK := dbc.Button(
+                    "OK",
+                    color="primary",
+                    outline=True,
+                    disabled=True,
+                )
+            ]
+        )
+    ],
+    size="xl",
+
+)
+
+
+
